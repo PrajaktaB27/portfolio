6075bc574d679b42d1367e1cb77bcac61af6794a
Prajakta Bimalkhedkar <prajakta.bimalkhedkar27@gmail.com>
2023-05-17 11:00:38 -0700

    portfolio setup

---
+++ src/portfolio.js
@@ -0,0 +1,68 @@
+const header = {
+  homepage: 'https://rajshekhar26.github.io/cleanfolio', 
+  //  change this link  
+}
+
+const about = {
+  name: 'Prajakta :)',
+  role: 'Software Engineer',
+  description:
+    'I am a recent Computer Science graduate with a strong foundation in software development and a passion for creating innovative solutions. Seeking a software developer/engineer role to leverage my technical skills in Python, JavaScript, and database management to contribute to cutting-edge projects and drive business growth!',
+  resume: 'https://drive.google.com/file/d/1d2gVd5nfnCro8Tn5CuXirUK2NA37_V3W/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/prajakta-bimalkhedkar-55b02817b/',
+    github: 'https://github.com/PrajaktaB27',
+  },
+}
+
+const projects = [
+  {
+    name: 'MetaDetector',
+    description:
+      'My team and I created the MetaDetector web app using the MEAN stack integrating Decentralands API to display precise information about virtual real estate in Decentraland. Our objective was to gain hands-on experience in full stack development and learn how SaaS applications are built.',
+    stack: ['MongoDB', 'Express', 'Angular', 'NodeJS'],
+    sourceCode: 'https://github.com/PrajaktaB27/SaaSProject---MetaDetector',
+    //  livePreview: 'https://github.com',  
+  },
+  {
+    name: 'TA Scheduling',
+    description:
+      'My team and I created an application that utilized a matching algorithm to assign TAs, graders, and tutors for courses, streamlining the scheduling process for the Computer Science Faculty at SeattleU each quarter.',
+    stack: ['Python', 'MongoDB', 'React', 'Cypress'],
+    sourceCode: 'https://github.com/PrajaktaB27/Graduate-Capstone---TA-Scheduling',
+    //  livePreview: 'https://github.com',  
+  },
+  {
+    name: 'MindScape',
+    description:
+      'A  mobile application UX design that offers guided meditations, breathing exercises, and stress-reducing techniques integrated with an AI chatbot. This project was developed as part of a graduate course in User Experience Design, Spring Quarter 2023 by a team of 4.',
+    stack: ['UX Design', 'Figma', 'Miro'],
+    sourceCode: 'https://github.com/PrajaktaB27/Mindscape---UX-Design/tree/main',
+    //  livePreview: 'https://github.com',  
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Angular',
+  'SASS',
+  'Python',
+  'Git',
+  'CI/CD',
+  'Pytest',
+  'MongoDB',
+  'Express',
+  'Cypress',
+  'Docker',
+  'AWS',
+]
+
+const contact = {
+  email: 'bimalkhedkarprajakta@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
